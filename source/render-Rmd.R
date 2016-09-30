85a3544948351f8ff4fb7977f490504c70d1b090
ThierryO <Thierry.Onkelinx@inbo.be>
2016-09-30 11:59:17 +0200

    add a script to render all Rmd files in the source folder

---
+++ source/render-Rmd.R
@@ -0,0 +1,11 @@
+to.render <- list.files(
+  path = "source",
+  pattern = "\\.Rmd$",
+  full.names = TRUE,
+  recursive = TRUE,
+  ignore.case = TRUE
+)
+
+for (i in to.render){
+  rmarkdown::render(i, output_dir = dirname(gsub("^source/", "", i)))
+}
